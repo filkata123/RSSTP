a15c570515f57767efb9bb1672d00915a8ff4393
Roshan Fernando <85596574+rfernandosfx@users.noreply.github.com>
2022-11-04 08:29:51 +0530

    PyTest for external files

--- src/external.py
+++ src/external.py
@@ -1,30 +1,35 @@
 class External:
     
     #Constructor of External
-    def_init_(self, n,p,l,o):
+    def _init_(self, n,p,l,o):
         self.n = n
         self.p = p
         self.l = l
         self.o = o
 
-    def getSensoryData():
+    def getSensoryData(self):
         #get inputs from existing sensor
         #Function with no input. The output is given by get_S().
         #import the sensory library and get the binary input,
         input = str(0b11100101)
+        
         # Extracting inputs we want 
-        return input[:, 4];
+        return input;
+        
 
-    def hitObstacle(p_input):
-        #get inputs from existing sensor
+    def hitObstacle(self):
+        # get inputs from existing sensor
+        return 0
 
-    def getPosition(self.l,self.p):
-        #This function returns p and also the actual geometry of the arm, i.e. the coor-dinates of the joints. 
-        #This can be computed using p and L, assuming that the first joint is in the origin (0, 0).
+    def getPosition(l,p):
+        # This function returns p and also the actual geometry of the arm, i.e. the coor-dinates of the joints. 
+        # This can be computed using p and L, assuming that the first joint is in the origin (0, 0).
+        return 0
 
-    def update(a):
-        #This function takes as an input an action a.
-        #The action a is an integer such that 0 ≤ a < 2n. If a = 2k for some k, then the k:th joint moves counterclockwise, 
+    def update(self):
+        # This function takes as an input an action a.
+        # The action a is an integer such that 0 ≤ a < 2n. If a = 2k for some k, then the k:th joint moves counterclockwise, 
         # and if a = 2k + 1 for some k, then the k:th joint moves clockwise.
+        return 0
 
     
--- tests/test_suite_example.py
+++ tests/test_suite_example.py
@@ -1,1 +1,16 @@
 # Example file for testing suite
+
+# Get the current working directory
+import os
+cwd = os.getcwd()
+
+import sys
+sys.path.append(cwd+'/src/')
+
+import external
+obj = external.External()
+
+def test_sensory_data():
+    
+    assert obj.getSensoryData() == '229'
+
