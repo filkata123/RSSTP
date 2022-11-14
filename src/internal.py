4f87e3f921521b225a1dfac2ebe43676ba2e14f3
Mourad Boulfrad <88975444+MouradBoulfrad@users.noreply.github.com>
2022-11-14 02:58:44 +0200

    Added implementation and tests for split and merge

--- src/internal.py
+++ src/internal.py
@@ -2,29 +2,50 @@ import numpy as np
 class Internal:
     
     #Constructor of Internal class
-    def _init_(self, mt, st):
+    def __init__(self, mt, st):
         self.transition_matrix = mt
         self.current_state = st
-
-    #TODO implement functions 
+    
     def split(self,n):
         #this function changes the transition graph G by splitting a node into two nodes. All the incoming and outgoing transitions are dublicated.
-        A = np.array([[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]])
-        return A
+        row = self.transition_matrix[n,:].reshape(1,np.shape(self.transition_matrix)[1])             #extract desired row
+        self.transition_matrix = np.vstack((self.transition_matrix,row))                             #add it to the matrix
+        col = self.transition_matrix[:,n].reshape(np.shape(self.transition_matrix)[0],1)             #extract desired col
+        self.transition_matrix = np.hstack((self.transition_matrix,col))                             #add it to the matrix
+        return self.transition_matrix
+
     def merge(self,n,m):
         #Opposite of split. Remove columns n, m and rows n, m and add instead one row and one column which are obtained by taking unions of elements of original rows/columns.
-        A = np.array([[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]])
-        return A
+        row1 = self.transition_matrix[n,:].reshape(1,np.shape(self.transition_matrix)[1])           #extract row n
+        row2 = self.transition_matrix[m,:].reshape(1,np.shape(self.transition_matrix)[1])           #extract row m
+        row1 += row2                                                                                #merge rows
+        for i in range(np.shape(self.transition_matrix)[1]):
+            row1[0,i]= tuple(set(row1[0,i]))                                                        #remove duplicates
+        self.transition_matrix[n,:] = row1.ravel()                                                  #update row n
+        self.transition_matrix = np.delete(self.transition_matrix,m, axis=0)                        #delete row m
+        col1 = self.transition_matrix[:,n].reshape(np.shape(self.transition_matrix)[0],1)           #extract column n
+        col2 = self.transition_matrix[:,m].reshape(np.shape(self.transition_matrix)[0],1)           #extract column m
+        col1 += col2                                                                                #merge columns
+        for i in range(np.shape(self.transition_matrix)[0]):
+            col1[i,0]= tuple(set(col1[i,0]))                                                        #remove duplicates
+        self.transition_matrix[:,n] = col1.ravel()                                                  #update column n
+        self.transition_matrix = np.delete(self.transition_matrix,m, axis=1)                        #remove column m                                                     
+        return self.transition_matrix
+
+    #TODO implement functions (remaining: add, delete, transition)
     def add(self,n,m,k):
         #add a connection from n to m with label k, meaning that anm := anm ∪ {k}.
-        A = np.array([[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]])
+        A = np.array([[['A11','B11','C11','D11'],['A12','B12','C12','D12']],[['A21','B21','C21','D21'],['A22','B22','C22','D22']]])
         return A
     def delete(self,n,m,k):
         #remove the connection from n to m, if it has label k, meaning that anm := anm \ {k}.
-        A = np.array([[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]])
+        A = np.array([[['A11','B11','C11','D11'],['A12','B12','C12','D12']],[['A21','B21','C21','D21'],['A22','B22','C22','D22']]])
         return A
     def transition(self,k):
         #This is a non-deterministic transition.
-        A = np.array([[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]])
+        A = np.array([[['A11','B11','C11','D11'],['A12','B12','C12','D12']],[['A21','B21','C21','D21'],['A22','B22','C22','D22']]])
         return A
         
+obj = Internal(np.array([[(0,1,2,3,4)]], dtype = object),0)
+obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+print(obj.split(2))
--- tests/internal_test.py
+++ tests/internal_test.py
@@ -1,32 +1,54 @@
 import os
 import sys
+import numpy as np
+import pytest
 cwd = os.getcwd()
 sys.path.append(cwd+'/src/')
 import internal
 
-obj = internal.Internal()
-
+obj = internal.Internal(np.array([[(0,1,2,3,4)]], dtype = object),0)
+# -------------------------------------------------------------Tests---------------------------------------------------------------------------------
 def test_split():
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
     n = 0
-    assert (obj.split(n) == [[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]]).all
+    assert (np.array_equal(obj.split(n) , np.array([[(0,1),(2,3),(4,3),(0,1)],[(1,2),(),(3,),(1,2)],[(2,4),(1,2,3),(1,),(2,4)],[(0,1),(2,3),(4,3),(0,1)]], dtype = object)))
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    n = 1
+    assert (np.array_equal(obj.split(n) , np.array([[(0,1),(2,3),(4,3),(2,3)],[(1,2),(),(3,),()],[(2,4),(1,2,3),(1,),(1,2,3)],[(1,2),(),(3,),()]], dtype = object)))
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    n = 2
+    assert (np.array_equal(obj.split(n) , np.array([[(0,1),(2,3),(4,3),(4,3)],[(1,2),(),(3,),(3,)],[(2,4),(1,2,3),(1,),(1,)],[(2,4),(1,2,3),(1,),(1,)]], dtype = object)))
 
 def test_merge():
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
     n = 0
-    m = 0
-    assert (obj.merge(n,m) == [[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]]).all
+    m = 1
+    assert (np.array_equal(obj.merge(n,m) , np.array([[(0,1,2,3),(3,4)],[(1,2,3,4),(1,)]], dtype = object)))
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    n = 0
+    m = 2
+    assert (np.array_equal(obj.merge(n,m) , np.array([[(0,1,2,3,4),(1,2,3)],[(1,2,3),()]], dtype = object)))
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    n = 1
+    m = 2
+    assert (np.array_equal(obj.merge(n,m) , np.array([[(0,1),(2,3,4)],[(1,2,4),(1,2,3)]], dtype = object)))
 
+
+# TODO test examples (remaining: add, delete, transition)
 def test_add():
     n = 0
     m = 0
     k = 0
-    assert (obj.add(n,m,k) == [[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]]).all
+    assert (obj.add(n,m,k) == [[['A11','B11','C11','D11'],['A12','B12','C12','D12']],[['A21','B21','C21','D21'],['A22','B22','C22','D22']]]).all
 
 def test_delete():
     n = 0
     m = 0
     k = 0
-    assert (obj.delete(n,m,k) == [[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]]).all
+    assert (obj.delete(n,m,k) == [[['A11','B11','C11','D11'],['A12','B12','C12','D12']],[['A21','B21','C21','D21'],['A22','B22','C22','D22']]]).all
 
 def test_transition():
     k = 0
-    assert (obj.transition(k) == [[['A','B','C','D'],['A','B','C','D']],[['A','B','C','D'],['A','B','C','D']]]).all
+    assert (obj.transition(k) == [[['A11','B11','C11','D11'],['A12','B12','C12','D12']],[['A21','B21','C21','D21'],['A22','B22','C22','D22']]]).all
+
+retcode = pytest.main()
