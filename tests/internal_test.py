02edfa1f56cd47b2688b682cf31bafccb154143e
Mourad Boulfrad <88975444+MouradBoulfrad@users.noreply.github.com>
2022-11-14 17:19:49 +0200

    formatted for better readability

--- tests/internal_test.py
+++ tests/internal_test.py
@@ -9,68 +9,155 @@ import internal
 obj = internal.Internal(np.array([[(0,1,2,3,4)]], dtype = object),0)
 # -------------------------------------------------------------Tests-------------------------------------------------------------------------------------------
 def test_split():
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
     n = 0
-    assert (np.array_equal(obj.split(n) , np.array([[(0,1),(2,3),(4,3),(0,1)],[(1,2),(),(3,),(1,2)],[(2,4),(1,2,3),(1,),(2,4)],[(0,1),(2,3),(4,3),(0,1)]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(2,3),(4,3),(0,1)],
+                         [(1,2),(),(3,),(1,2)],
+                         [(2,4),(1,2,3),(1,),(2,4)],
+                         [(0,1),(2,3),(4,3),(0,1)]], dtype = object)
+    assert (np.array_equal(obj.split(n) , expected))
+
     n = 1
-    assert (np.array_equal(obj.split(n) , np.array([[(0,1),(2,3),(4,3),(2,3)],[(1,2),(),(3,),()],[(2,4),(1,2,3),(1,),(1,2,3)],[(1,2),(),(3,),()]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(2,3),(4,3),(2,3)],
+                         [(1,2),(),(3,),()],
+                         [(2,4),(1,2,3),(1,),(1,2,3)],
+                         [(1,2),(),(3,),()]], dtype = object)
+    assert (np.array_equal(obj.split(n) , expected))
+    
     n = 2
-    assert (np.array_equal(obj.split(n) , np.array([[(0,1),(2,3),(4,3),(4,3)],[(1,2),(),(3,),(3,)],[(2,4),(1,2,3),(1,),(1,)],[(2,4),(1,2,3),(1,),(1,)]], dtype = object)))
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(2,3),(4,3),(4,3)],
+                         [(1,2),(),(3,),(3,)],
+                         [(2,4),(1,2,3),(1,),(1,)],
+                         [(2,4),(1,2,3),(1,),(1,)]], dtype = object)
+    assert (np.array_equal(obj.split(n) , expected))
 
 def test_merge():
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
     n = 0
     m = 1
-    assert (np.array_equal(obj.merge(n,m) , np.array([[(0,1,2,3),(3,4)],[(1,2,3,4),(1,)]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1,2,3),(3,4)],
+                         [(1,2,3,4),(1,)]], dtype = object)
+    assert (np.array_equal(obj.merge(n,m) , expected ))
+    
     n = 0
     m = 2
-    assert (np.array_equal(obj.merge(n,m) , np.array([[(0,1,2,3,4),(1,2,3)],[(1,2,3),()]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object) 
+
+    expected = np.array([[(0,1,2,3,4),(1,2,3)],
+                         [(1,2,3),()]], dtype = object)
+    assert (np.array_equal(obj.merge(n,m) ,expected ))
+
     n = 1
     m = 2
-    assert (np.array_equal(obj.merge(n,m) , np.array([[(0,1),(2,3,4)],[(1,2,4),(1,2,3)]], dtype = object)))
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
 
+    expected = np.array([[(0,1),(2,3,4)],
+                         [(1,2,4),(1,2,3)]], dtype = object)
+    assert (np.array_equal(obj.merge(n,m) , expected ))
 
 def test_add():
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
     n = 0
     m = 1
     k = 0
-    assert (np.array_equal(obj.add(n,m,k) , np.array([[(0,1),(0,2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(0,2,3),(4,3)],
+                         [(1,2),(),(3,)],
+                         [(2,4),(1,2,3),(1,)]], dtype = object)
+    assert (np.array_equal(obj.add(n,m,k) , expected))
+
     n = 1
     m = 1
     k = 3
-    assert (np.array_equal(obj.add(n,m,k) , np.array([[(0,1),(2,3),(4,3)],[(1,2),(3,),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(2,3),(4,3)],
+                         [(1,2),(3,),(3,)],
+                         [(2,4),(1,2,3),(1,)]], dtype = object)
+    assert (np.array_equal(obj.add(n,m,k) , expected ))
+
     n = 2
     m = 2
     k = 0
-    assert (np.array_equal(obj.add(n,m,k) , np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(0,1)]], dtype = object)))
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(2,3),(4,3)],
+                         [(1,2),(),(3,)],
+                         [(2,4),(1,2,3),(0,1)]], dtype = object)
+    assert (np.array_equal(obj.add(n,m,k) , expected))
+
 def test_delete():
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
     n = 0
     m = 0
     k = 0
-    assert (np.array_equal(obj.delete(n,m,k) , np.array([[(1,),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(1,),(2,3),(4,3)],
+                         [(1,2),(),(3,)],
+                         [(2,4),(1,2,3),(1,)]], dtype = object)
+    assert (np.array_equal(obj.delete(n,m,k) , expected))
+
     n = 1
     m = 2
     k = 3
-    assert (np.array_equal(obj.delete(n,m,k) , np.array([[(0,1),(2,3),(4,3)],[(1,2),(),()],[(2,4),(1,2,3),(1,)]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(2,3),(4,3)],
+                         [(1,2),(),()],
+                         [(2,4),(1,2,3),(1,)]], dtype = object)
+    assert (np.array_equal(obj.delete(n,m,k) , expected))
+
     n = 2
     m = 1
     k = 2
-    assert (np.array_equal(obj.delete(n,m,k) , np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,3),(1,)]], dtype = object)))
-    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(2,3),(4,3)],
+                         [(1,2),(),(3,)],
+                         [(2,4),(1,3),(1,)]], dtype = object)
+    assert (np.array_equal(obj.delete(n,m,k) , expected))
+
     n = 2
     m = 1
     k = 0
-    assert (np.array_equal(obj.delete(n,m,k) , np.array([[(0,1),(2,3),(4,3)],[(1,2),(),(3,)],[(2,4),(1,2,3),(1,)]], dtype = object)))
+    obj.transition_matrix = np.array([[(0,1),(2,3),(4,3)],
+                                      [(1,2),(),(3,)],
+                                      [(2,4),(1,2,3),(1,)]], dtype = object)
+
+    expected = np.array([[(0,1),(2,3),(4,3)],
+                         [(1,2),(),(3,)],
+                         [(2,4),(1,2,3),(1,)]], dtype = object)
+    assert (np.array_equal(obj.delete(n,m,k) , expected))
 
 # TODO test examples (remaining: transition)
 def test_transition():
