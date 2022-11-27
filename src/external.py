9749d8cf79aa155c43920ec7e66d4e386f9df56a
khalil chakal <khacha329@gmail.com>
2022-11-27 17:08:51 +0200

    not tested n joints

--- src/external.py
+++ src/external.py
@@ -19,46 +19,69 @@ class External:
         self.p = p
         self.feedback = feedback
 
-        if self.__hit_obstacle(self._calculate_coordinates(self.l, self.p)):
-            raise Exception("Initial position must not intersect with objects")
+        if self.__hit_obstacle(self.p):
+            raise Exception("Initial position must not intersect with obstacles")
 
     # Private methods 
-    def _calculate_coordinates(self, length, position):
-        x = round(length*math.cos(math.radians(position)),2)
-        y = round(length*math.sin(math.radians(position)),2)
 
-        return (x,y)
+    def _calculate_coordinates(self, length, position,joint_nbr):
+        x,y=0
+        for i in range(joint_nbr):
+            temp_x = round(length[i]*math.cos(math.radians(position[i])),2)
+            temp_y = round(length[i]*math.sin(math.radians(position[i])),2)
+            x+= temp_x
+            y+= temp_y
 
-    def __hit_obstacle(self, coordinates):
+        return (x,y)  # this will always return x,y for joint i
+
+    def __hit_obstacle(self, new_position):
         # this will create 2 circles at (1,2) and (-1,-2) with radii 1
         # idealy when we create instance of class we fill in obstacles 
         # make use of self.o to create obs
-
-        #TODO: list of obstacles and less hard-coding
-        #TODO(2): What if no objects?
-
-        center1 = Point(self.o[0][0],self.o[0][1])
-        circle1 = center1.buffer(self.o[1]).boundary
-
-        center2 = Point(self.o[2][0],self.o[2][1])
-        circle2 = center2.buffer(self.o[3]).boundary
-        
-        #constructing of 1 arm with 1 node at origin  with length l
-        line = LineString([(0,0), (coordinates[0], coordinates[1])])
-
-        #checks if line and any of the circles intersect
-        obstacle1 = circle1.intersects(line)
-        obstacle2 = circle2.intersects(line)
-
-        if obstacle1 or obstacle2:
-            return True
-        else:
+        coordinates_2=[]
+        line=[]
+        obstacle=[]
+        center=[]
+        circle=[]
+        nbr_of_obstacles = len(self.o)/2
+        nbr_of_lines = self.n
+        size_of_bool = nbr_of_lines * nbr_of_obstacles
+        #TODO: list of obstacles and less hard-coding #Done
+        #TODO(2): What if no objects? #Done
+        if len(self.o) == 0:
+            print ("No obstacles in environment")
             return False
+        else:    
+            for i in range(len(self.o),2):
+
+                center[i] = Point(self.o[i][0],self.o[i][1])
+                circle[i] = center[i].buffer(self.o[i+1]).boundary
+
+
+        for i in range(self.n):
+            #constructing of 1 arm with 1 node at origin  with length l
+            coordinates_2[i] = self._calculate_coordinates(self.l[i],new_position[i],i) 
+            if i > 1:
+                line[i] = LineString([(coordinates_2[i-1][0], coordinates_2[i-1][1]), (coordinates_2[i][0], coordinates_2[i][1])])
+            else:
+                line[i] = LineString((0,0), (coordinates_2[i][0], coordinates_2[i][1]))
+
+        for i in range(nbr_of_obstacles):
+            for j in range(nbr_of_lines):
+                obstacle[nbr_of_lines*i+j] = circle[i].intersects(line[j])   
+            #checks if line and any of the circles intersect
+
+        for i in range(size_of_bool): #nbr of obstacles * nbr of lines/arms
+            if obstacle[i]:
+                return True
+            else:
+                return False
 
     # Public methods
     def get_sensory_data(self):
         #get inputs from existing sensor
         #Function with no input. The output is given by get_S().
+        #TODO: Figure out what is feedback for n joints
         if self._calculate_coordinates(self.l, self.p) == self.feedback:      
             return 1
         else:
@@ -67,29 +90,32 @@ class External:
     def get_position(self):
         # This function returns p and also the actual geometry of the arm, i.e. the coordinates of the joints. 
         # This can be computed using p and L, assuming that the first joint is in the origin (0, 0).
-        
-        #TODO: coordinates for multiple joints
-        coordinates = self._calculate_coordinates(self.l, self.p)
-        return (self.p, coordinates)
+        coordinates=()
+        #TODO: coordinates for multiple joints #Done
+        for i in range(self.n):
+            coordinates[i] = self._calculate_coordinates(self.l, self.p,i)
+            return (self.p, coordinates)
 
 
     def update(self, a):
-        new_position=self.p
+        new_position=[]
+        for i in range(self.n):
+            new_position[i]=self.p[i]
         # This function takes as an input an action a from internal library.
         # The action a is an integer such that 0 ≤ a < 2n. If a = 2k for some k, then the k:th joint moves counterclockwise, 
         # and if a = 2k + 1 for some k, then the k:th joint moves clockwise.
 
-        #TODO: multiple joints
+        #TODO: multiple joints #Done
         if a % 2 == 0: #move clockwise
-            #k = int(a/2) 
-            new_position = (new_position + 1) % 360
+            k = int(a/2) 
+            new_position[k] = (new_position[k] + 1) % 360
         else: #move counterclockwise
-           # k = int((a-1)/2)
-            new_position = (new_position - 1) % 360
+            k = int((a-1)/2)
+            new_position[k] = (new_position[k] - 1) % 360
 
-        coordinates_after_move = self._calculate_coordinates(self.l, new_position)
+        coordinates_after_move = self._calculate_coordinates(self.l, new_position,k)
 
-        if not self.__hit_obstacle(coordinates_after_move):
-            self.p = new_position # move arm to new position
+        if not self.__hit_obstacle(new_position):
+            self.p[k] = new_position[k] # move joint to new position
         else:
-            print('Position (' + str(coordinates_after_move) + ') unreachable due to object! \n')
+            print('Position of joint '+ k +' is (' + str(coordinates_after_move) + ') unreachable due to object! \n')
