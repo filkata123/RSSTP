920f2e0d30663880a5a69d724a3ea683aa5c3ac4
Roshan Fernando <115605256+RoshanFernandoOulu@users.noreply.github.com>
2022-11-30 13:21:33 +0200

    Update demo_external.py
    Coordinates used Khalil for testing

--- src/demo_external.py
+++ src/demo_external.py
@@ -1,9 +1,23 @@
 from external import External
 
-n = 2
-p = [95,80]
-l = [2,3]
-o = [[1,2],1,[-1,-2],1]
+# n = 2
+# p = [95,80]
+# l = [2,3]
+# o = [[1,2],1,[-1,-2],1]
+# feedback = (0.38, 4.95)
+# obj = External(n, p, l, o, feedback)
+
+# n = 2
+# p = [26.56, 45.00]
+# l = [4.47,5.65]
+# o = [[5,4],1,[-2,-2],1]
+# feedback = (0.38, 4.95)
+# obj = External(n, p, l, o, feedback)
+
+n = 3
+p = [120, 108.43, 180]
+l = [2.82, 9.48,4.00]
+o = [[3,3],1,[-8,9],1, [-5,-3], 1]
 feedback = (0.38, 4.95)
 obj = External(n, p, l, o, feedback)
 
